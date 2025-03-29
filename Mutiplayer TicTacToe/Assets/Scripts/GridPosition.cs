ff2addd3095cb00e596fa91fdb0a3845760180bf
Rajan <themailboxofabi@yahoo.com>
2025-03-29 22:14:14 +0530

    Mouse click detection using IPointerDownHandler and OnMouseDown
    Implemented click detection using regular OnMouseDown and also IPointerDownHandler.
    
    Added physics 2d raycaster to the main camera.
    Added an EventSystem.

---
+++ Mutiplayer TicTacToe/Assets/Scripts/GridPosition.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GridPosition : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("On Ponter Down!");
+    }
+
+    // add a raycaster to the main camera. Physics 2D raycaster is what is going to allow that function to be called. We also need an EventSystem.
+    
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Click");
+       
+    }
+
+
+}
+
+
