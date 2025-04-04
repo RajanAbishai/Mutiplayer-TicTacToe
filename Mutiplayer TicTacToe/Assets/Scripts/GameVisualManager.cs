1afe0ba5b9b04560582ed824a70da6cb7b937a49
Rajan <themailboxofabi@yahoo.com>
2025-04-04 23:39:00 +0530

    Circle and Cross prefabs added to GameVisualManager script

---
+++ Mutiplayer TicTacToe/Assets/Scripts/GameVisualManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameVisualManager : MonoBehaviour
+{
+
+    [SerializeField] private Transform crossPrefab;  //can also say private GameObject
+    [SerializeField] private Transform circlePrefab;
+    
+
+
+}
